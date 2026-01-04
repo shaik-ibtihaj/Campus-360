638839ade05381f4987e98ab2772b178b310dbdf
yaza5671 <shogo5671@gmail.com>
2026-01-04 18:41:56 +0100

    Update SDK and Java version in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,8 +6,8 @@ plugins {
 
 android {
     namespace = "com.example.campus360"
-    compileSdk {
-        version = release(36)
+    compileSdk = 36
+    targetSdk = 36
     }
 
     defaultConfig {
@@ -30,11 +30,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
